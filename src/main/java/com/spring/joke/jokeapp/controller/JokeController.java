36fd7285fcc9b9514ae6f113ca44ee7afb89ad37
ilya <i.dovzhenko26@gmail.com>
2019-11-13 14:28:03 +0700

    add view layer

--- src/main/java/com/spring/joke/jokeapp/controller/JokeController.java
+++ src/main/java/com/spring/joke/jokeapp/controller/JokeController.java
@@ -4,7 +4,7 @@ import com.spring.joke.jokeapp.services.JokeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class JokeController {
@@ -15,7 +15,7 @@ public class JokeController {
         this.jokeService = jokeService;
     }
 
-    @PostMapping({"/",""})
+    @GetMapping({"/",""})
     public String showJoke(Model model) {
         model.addAttribute("joke", jokeService.getJoke());
         return "chuck-norris";
